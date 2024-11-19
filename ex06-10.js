24a1855e5362f279d732b13293e9faba9b7487c1
ryoutoisoda200040624 <168411148+ryoutoisoda200040624@users.noreply.github.com>
2024-11-19 09:59:08 +0900

    Update ex06-10.js

--- ex06-10.js
+++ ex06-10.js
@@ -1,2 +1,4 @@
 function imgchange1(){document.getElementById('logo').src='./image/happakomarigao.png';}
 function imgchange2(){document.getElementById('logo').src='./image/happamotonokao.png';}
+function imgchange3(){document.getElementById('logo1').src='./image/josaigazo.png';}
+function imgchange4(){document.getElementById('logo1').src='./image/josairogo.png';}
