ca8e8253527873d33ab9f46992cfa688e8369741
ryoutoisoda200040624 <168411148+ryoutoisoda200040624@users.noreply.github.com>
2024-11-26 13:51:12 +0900

    Update ex06-10.js

--- ex06-10.js
+++ ex06-10.js
@@ -2,3 +2,21 @@ function imgchange1(){document.getElementById('logo').src='./image/happakomariga
 function imgchange2(){document.getElementById('logo').src='./image/happamotonokao.png';}
 function imgchange3(){document.getElementById('logo1').src='./image/josaigazo.png';}
 function imgchange4(){document.getElementById('logo1').src='./image/josairogo.png';}
+
+
+
+function hyoji(){
+  //表示する文字
+  var str = "一文字ずつ表示します。";
+  //テキストボックスの文字数
+  var cnt = document.timer.moji.value.length;
+  //文字が全部表示されているか確認
+if( cnt < 11){
+    //現在より1文字多く切り出して表示
+   document.timer.moji.value=str.substr(0,cnt+1);}
+  else{
+    //全て表示されたら、空文字に戻す
+  document.timer.moji.value="";}}
+function startfnc(){
+  //関数hyoji()を1000ミリ秒間隔で呼び出す
+  setlnterval("hyoji()",1000;}
