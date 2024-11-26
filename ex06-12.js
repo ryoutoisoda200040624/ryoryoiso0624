44f01f38ac180f982bfc578028a5f72048cf9869
ryoutoisoda200040624 <168411148+ryoutoisoda200040624@users.noreply.github.com>
2024-11-26 10:33:53 +0900

    Update ex06-12.js

--- ex06-12.js
+++ ex06-12.js
@@ -4,7 +4,7 @@ function hyoji(){
   //テキストボックスの文字数
   var cnt=document.timer.moji.value.length;
   //文字が全部表示されているか確認
-  if(cnt<11){
+  if(cnt < 11){
     //現在より1文字多く切り出して表示
     document.timer.moji.value=str.substr(0,cnt+1);}
   else{
