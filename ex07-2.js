a01eda153a5b3fb8ccd407b75d242a8175bbdfb3
ryoutoisoda200040624 <168411148+ryoutoisoda200040624@users.noreply.github.com>
2024-12-10 14:02:53 +0900

    Update ex07-2.js

--- ex07-2.js
+++ ex07-2.js
@@ -1,7 +1,9 @@
+Number('x');
+Number('y');
+
 function add(){ 
   var x =document.getElementById('input1').value;
   var y =document.getElementById('input2').value;
   alert(x+y);
 }
-Number('x');
-Number('y');
+
