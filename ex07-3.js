befbb965e7d3c31978e72e4e85207c550375276f
ryoutoisoda200040624 <168411148+ryoutoisoda200040624@users.noreply.github.com>
2024-12-10 14:28:34 +0900

    Update ex07-3.js

--- ex07-3.js
+++ ex07-3.js
@@ -1,5 +1,5 @@
 function BMI(){ 
   var x =document.getElementById('input1').value;
   var y =document.getElementById('input2').value;
-  alert(Number('x')/(Number('y')**2));
+  alert(Number(x)/(Number(y)**2));
 }
