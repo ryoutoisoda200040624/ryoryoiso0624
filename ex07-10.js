b782eb3e782c91fbf491ad44402dc2315db96fd7
ryoutoisoda200040624 <168411148+ryoutoisoda200040624@users.noreply.github.com>
2024-12-24 15:25:40 +0900

    Update ex07-10.js

--- ex07-10.js
+++ ex07-10.js
@@ -2,4 +2,6 @@ function BMI2(){
   var x =document.getElementById('input10').value;
   var y =document.getElementById('input11').value;
   alert(Number(x)/(Number(y)**2));
+if(document.getElementById('answer10').value=='<18.5'){
+  alert('低体重')}
 }
