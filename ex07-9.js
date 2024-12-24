7f5c18776d1d2f43653efcf4bb528714756101b6
ryoutoisoda200040624 <168411148+ryoutoisoda200040624@users.noreply.github.com>
2024-12-24 14:49:42 +0900

    Update ex07-9.js

--- ex07-9.js
+++ ex07-9.js
@@ -1,11 +1,11 @@
-function nazo79()
+function nazo79(){
   if(document.getElementById('answer79').value=='夢')
   {
     alert('あたり！');
-  }else if(document.getElementById('answer79').value=='ゆめ'
+  }else if(document.getElementById('answer79').value=='ゆめ')
            {
     alert('あたり！');
   }else{
     alert('はずれ！')
   }
-
+}
