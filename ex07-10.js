07ebbc826bfd6428257b11eaa695b452618eae57
ryoutoisoda200040624 <168411148+ryoutoisoda200040624@users.noreply.github.com>
2025-01-07 09:32:01 +0900

    Update ex07-10.js

--- ex07-10.js
+++ ex07-10.js
@@ -2,6 +2,8 @@ function BMI2(){
   var x =document.getElementById('input10').value;
   var y =document.getElementById('input11').value;
   alert(Number(x)/(Number(y)**2));
+}
+  function taiju(){
 if(document.getElementById('answer10').value=='<18.5'){
   alert('低体重')}
 }
