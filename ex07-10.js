f51e52a7739fd87ea5e4aeaf4b8c26c318cd188a
ryoutoisoda200040624 <168411148+ryoutoisoda200040624@users.noreply.github.com>
2025-01-19 17:55:15 +0900

    Update ex07-10.js

--- ex07-10.js
+++ ex07-10.js
@@ -8,8 +8,7 @@ if(document.getElementById('answer11').value=='Number(x)/(Number(y)**2)<18.5'){
   alert('低体重')}
     else if(document.getElementById('answer12').value=='Number(x)/(Number(y)**2)>=25'){
             alert('肥満')}
-    else if(document.getElementById('answer13').value=='18.5=<Number(x)/(Number(y)**2)<25')
-         {alert('普通')}
+    
     else{
-      alert('不明')}
+      alert('普通')}
 }
