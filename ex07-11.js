8e752aec79500356fea70f4839f66753a669adda
ryoutoisoda200040624 <168411148+ryoutoisoda200040624@users.noreply.github.com>
2025-01-20 23:05:15 +0900

    Update ex07-11.js

--- ex07-11.js
+++ ex07-11.js
@@ -1,7 +1,8 @@
 function uruu(){
   if(document.getElementById('seireki').value/400){
 alert('閏年');
-  }else if(document.getElementById('seireki').value/100{
+  }else if(document.getElementById('seireki').value/100
+  {
     alert('平年');
   }else if(document.getElementById('seireki').value/4{
     alert('閏年');
