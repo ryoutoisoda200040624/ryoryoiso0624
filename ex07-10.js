fcf280075a3b890db2620da061716c77af36181a
ryoutoisoda200040624 <168411148+ryoutoisoda200040624@users.noreply.github.com>
2025-01-20 18:48:38 +0900

    Update ex07-10.js

--- ex07-10.js
+++ ex07-10.js
@@ -1,15 +1,12 @@
-function BMI2(){ 
-  var x =document.getElementById('input10').value;
-  var y =document.getElementById('input11').value;
-  alert(Number(x)/(Number(y)**2));
-}
+
   function taiju(){
     var x =document.getElementById('input10').value;
   var y =document.getElementById('input11').value;
     alert(Number(x)/(Number(y)**2));
-if(Number(x)/(Number(y)**2).value<18.5){
+    var nan =Number(x)/(Number(y)**2).value;
+if(nan.value<18.5){
   alert('低体重')}
-    else if(Number(x)/(Number(y)**2).value>=25){
+    else if(nan.value>=25){
             alert('肥満')}
     
     else{
