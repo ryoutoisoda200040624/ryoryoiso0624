4732fbbafd3cd35b31c6fb2e4c964632a76efb30
ryoutoisoda200040624 <168411148+ryoutoisoda200040624@users.noreply.github.com>
2025-01-20 18:38:52 +0900

    Update ex07-10.js

--- ex07-10.js
+++ ex07-10.js
@@ -6,6 +6,7 @@ function BMI2(){
   function taiju(){
     var x =document.getElementById('input10').value;
   var y =document.getElementById('input11').value;
+    alert(Number(x)/(Number(y)**2));
 if(Number(x)/(Number(y)**2).value<18.5){
   alert('低体重')}
     else if(Number(x)/(Number(y)**2).value>=25){
