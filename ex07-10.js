9f2c1bbfae4853defd926da0331bd5794839265f
ryoutoisoda200040624 <168411148+ryoutoisoda200040624@users.noreply.github.com>
2025-01-26 12:30:12 +0900

    Update ex07-10.js

--- ex07-10.js
+++ ex07-10.js
@@ -5,9 +5,9 @@
     alert(Number(x)/(Number(y)**2));
 
   
-if( document.getElementById('answer10').value < 18.5 ){
+if( document.getElementById('nani1').value < 18.5 ){
   alert('低体重')}
-    else if( document.getElementById('answer10').value >= 25 ){
+    else if( document.getElementById('nani1').value >= 25 ){
             alert('肥満')}
     
     else{
